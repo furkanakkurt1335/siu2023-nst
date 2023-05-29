04c69adab6506fa823151a0b1943f66e8a7d5fe2
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-05-29 12:55:41 +0300

    add data

--- src/aggregate_data.py
+++ src/aggregate_data.py
@@ -4,11 +4,11 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(THIS_DIR, '..', 'data')
 raw_path = os.path.join(data_path, 'raw')
 clean_path = os.path.join(data_path, 'clean')
-data_files = [i for i in os.listdir(clean_path) if i.endswith('.json')]
+data_files = [i for i in os.listdir(raw_path) if i.endswith('.json')]
 
 data_d = {}
 for data_file in data_files:
-    with open(os.path.join(clean_path, data_file), encoding='utf-8') as f:
+    with open(os.path.join(raw_path, data_file), encoding='utf-8') as f:
         data = json.load(f)
     for el in data:
         tweet_id = el['TweetID']
@@ -21,5 +21,5 @@ for data_file in data_files:
                 if key not in data_d[tweet_id].keys():
                     data_d[tweet_id][key] = el[key]
 
-with open(os.path.join(data_path, 'aggregated-data.json'), 'w', encoding='utf-8') as f:
+with open(os.path.join(data_path, 'aggregated-data-raw.json'), 'w', encoding='utf-8') as f:
     json.dump(data_d, f, indent=4, ensure_ascii=False)
