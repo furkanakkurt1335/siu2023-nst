b8a4940767897862644000e0d30cb6fc0af047e7
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-05-29 12:42:13 +0300

    update scripts

--- src/aggregate_data.py
+++ src/aggregate_data.py
@@ -1,12 +1,14 @@
 import os, json
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(THIS_DIR, '..', 'data', 'clean')
-data_files = [i for i in os.listdir(data_path) if i.endswith('.json')]
+data_path = os.path.join(THIS_DIR, '..', 'data')
+raw_path = os.path.join(data_path, 'raw')
+clean_path = os.path.join(data_path, 'clean')
+data_files = [i for i in os.listdir(clean_path) if i.endswith('.json')]
 
 data_d = {}
-for file in data_files:
-    with open(os.path.join(data_path, file), encoding='utf-8') as f:
+for data_file in data_files:
+    with open(os.path.join(clean_path, data_file), encoding='utf-8') as f:
         data = json.load(f)
     for el in data:
         tweet_id = el['TweetID']
@@ -19,5 +21,5 @@ for file in data_files:
                 if key not in data_d[tweet_id].keys():
                     data_d[tweet_id][key] = el[key]
 
-with open(os.path.join(THIS_DIR, 'aggregated-data.json'), 'w', encoding='utf-8') as f:
+with open(os.path.join(data_path, 'aggregated-data.json'), 'w', encoding='utf-8') as f:
     json.dump(data_d, f, indent=4, ensure_ascii=False)
--- src/clean.py
+++ src/clean.py
@@ -2,7 +2,8 @@ import os, json, re
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(THIS_DIR, '..', 'data')
-data_files = [i for i in os.listdir(data_path) if i.endswith('.json')]
+raw_path = os.path.join(data_path, 'raw')
+data_files = [i for i in os.listdir(raw_path) if i.endswith('.json')]
 clean_path = os.path.join(data_path, 'clean')
 if not os.path.exists(clean_path):
     os.mkdir(clean_path)
@@ -10,7 +11,7 @@ if not os.path.exists(clean_path):
 url_pattern = 'https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)' # taken from https://uibakery.io/regex-library/url
 twitter_user_pattern = '@[a-zA-Z0-9_]{1,15}'
 for file in data_files:
-    with open(os.path.join(data_path, file), encoding='utf-8') as f:
+    with open(os.path.join(raw_path, file), encoding='utf-8') as f:
         data = json.load(f)
     data_len = len(data)
     new_data = []
@@ -26,8 +27,9 @@ for file in data_files:
             text = re.sub(twitter_user_pattern, '', text) # remove twitter users
             while '  ' in text:
                 text = text.replace('  ', ' ')
-            el['text'] = text.strip()
-            new_data.append(el)
-            continue
+            text = text.strip()
+            if text:
+                el['text'] = text.strip()
+                new_data.append(el)
     with open(os.path.join(clean_path, file), 'w', encoding='utf-8') as f:
         json.dump(new_data, f, indent=4, ensure_ascii=False)
