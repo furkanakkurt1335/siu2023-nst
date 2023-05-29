636ae90b915e9c788979dadc24484ea6d212152c
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-05-29 17:12:04 +0300

    update train script

---
+++ src/models.md
@@ -0,0 +1,8 @@
+# Models
+
+Tried:
+
+- sklearn.linear_model.LogisticRegression
+- sklearn.naive_bayes.MultinomialNB
+- sklearn.tree.DecisionTreeClassifier
+- sklearn.ensemble.RandomForestClassifier
--- src/train.py
+++ src/train.py
@@ -15,21 +15,27 @@ with open(path, encoding='utf-8') as f:
     data = json.load(f)
 
 feature = args.feature
-print('Feature: {}'.format(feature))
+out_d = {'feature': feature}
 
 corpus = [el['text'] for el in data if feature in el.keys()]
-print('Corpus length: {}'.format(len(corpus)))
+# print('Corpus length: {}'.format(len(corpus)))
 
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(3, 3), max_features=1000, lowercase=True)
+# {'feature': 'existence', 'train_score': '0.8014', 'test_score': '0.7644'}
+# {'feature': 'category', 'train_score': '0.7368', 'test_score': '0.7066'}
+# {'feature': 'strength', 'train_score': '0.7607', 'test_score': '0.7135'}
+
 X = vectorizer.fit_transform(corpus)
-print('Feature names: {}'.format(vectorizer.get_feature_names_out()))
-print('Shape: {}'.format(X.shape))
+# print('Feature names: {}'.format(vectorizer.get_feature_names_out()))
+# print('Shape: {}'.format(X.shape))
 
 y = [el[feature] for el in data if feature in el.keys()]
-print('Labels length: {}'.format(len(y)))
+# print('Labels length: {}'.format(len(y)))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 clf = LogisticRegression(random_state=0).fit(X_train, y_train)
-print('Train score: {}'.format(clf.score(X_train, y_train)))
-print('Test score: {}'.format(clf.score(X_test, y_test)))
+out_d['train_score'] = '{:.4f}'.format(clf.score(X_train, y_train))
+out_d['test_score'] = '{:.4f}'.format(clf.score(X_test, y_test))
+# print(clf.predict_proba(X_test[:10])) # regression
+print(out_d)
