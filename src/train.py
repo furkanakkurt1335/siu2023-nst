0a10c0611368bed836a001dd28a529b394824b43
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-06-01 12:21:24 +0300

    update scripts

--- src/train.py
+++ src/train.py
@@ -6,9 +6,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--feature', type=str, default='existence', choices=['existence', 'strength', 'category'], help='Feature to train on')
-parser.add_argument('--type', type=str, default='all', choices=['all', 'isr-pal', 'refugee', 'tr-gr'], help='Data type to train on')
-parser.add_argument('--subtask', type=str, help='Subtask to test', choices=['1', '2', '3', '4'])
+parser.add_argument('-f', '--feature', type=str, default='existence', choices=['existence', 'strength', 'category'], help='Feature to train on')
+parser.add_argument('-t', '--type', type=str, default='all', choices=['all', 'isr-pal', 'refugee', 'tr-gr'], help='Data type to train on')
+parser.add_argument('-s', '--subtask', type=str, help='Subtask to test', choices=['1', '2', '3', '4'])
 args = parser.parse_args()
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -29,9 +29,7 @@ else:
     with open(test_file, encoding='utf-8') as f:
         test_data = json.load(f)
     test_data_l = [el for el in test_data if args.subtask in el['subtask']]
-    test_texts = [el['text'] for el in test_data_l]
     print('Test data loaded')
-    print('Test data length: {}'.format(len(test_texts)))
     subtask = args.subtask
     if subtask == '1':
         feature = 'category'
@@ -49,26 +47,31 @@ if args.type != 'all':
     print('Using data of type {}'.format(args.type))
     corpus = [el['text'] for el in data if el['type'] == args.type and feature in el.keys()]
     y = [el[feature] for el in data if el['type'] == args.type and feature in el.keys()]
+    test_corpus = [el['text'] for el in test_data_l if el['type'] == args.type]
 else:
     print('Using all data')
-    y = [el[feature] for el in data if feature in el.keys()]
     corpus = [el['text'] for el in data if feature in el.keys()]
+    y = [el[feature] for el in data if feature in el.keys()]
+    test_corpus = [el['text'] for el in test_data_l]
+
+print('Test data length: {}'.format(len(test_corpus)))
 
 max_features = 3000
-vectorizer1 = TfidfVectorizer(analyzer='char', ngram_range=(3, 3), max_features=max_features, lowercase=True, strip_accents='unicode')
-vectorizer2 = TfidfVectorizer(max_features=max_features, lowercase=True, strip_accents='unicode')
+vectorizer1 = TfidfVectorizer(analyzer='char', ngram_range=(3, 3), max_features=max_features)
+vectorizer2 = TfidfVectorizer(max_features=max_features)
 
 X_train = vectorizer1.fit_transform(corpus)
 X_t = vectorizer2.fit_transform(corpus)
 X_train = np.concatenate((X_train.toarray(), X_t.toarray()), axis=1)
 
 if test:
-    X_test = vectorizer1.transform(test_texts)
-    X_test_t = vectorizer2.transform(test_texts)
+    X_test = vectorizer1.transform(test_corpus)
+    X_test_t = vectorizer2.transform(test_corpus)
     X_test = np.concatenate((X_test.toarray(), X_test_t.toarray()), axis=1)
 
 if not test:
     X_train, X_test, y_train, y_test = train_test_split(X_train, y, test_size=0.2, random_state=42)
+    print('Splitting train data into train and test sets')
 else:
     X_train, y_train = X_train, y
 
